921f62c66a08236a293d003b1b76aa3977782024
Aniyah McWilliams <ybf3jw@virginia.edu>
2024-04-30 18:41:52 +0000

    my code does everything i just need to double check with the professor

--- get-message.py
+++ get-message.py
@@ -6,18 +6,9 @@ import json
 # Set up your SQS queue URL and boto3 client
 url = "https://sqs.us-east-1.amazonaws.com/440848399208/ybf3jw"
 sqs = boto3.client('sqs')
-"""
-def delete_message(handle):
-    try:
-        # Delete message from SQS queue
-        sqs.delete_message(
-            QueueUrl=url,
-            ReceiptHandle=handle
-        )
-        print("Message deleted")
-    except ClientError as e:
-        print(e.response['Error']['Message'])
-        """
+
+
+messages_dict = {}
 
 def get_message():
     message_amount = 10
@@ -45,18 +36,44 @@ def get_message():
                 handle = response['Messages'][0]['ReceiptHandle']
 
                 # Print the message attributes - this is what you want to work with to reassemble the message
-                print(f"Order: {order}")
-                print(f"Word: {word}")
+               # print(f"Order: {order}")
+                #print(f"Word: {word}")
+
+                message = {order: word}
+
+                messages_dict.update(message)
 
             # If there is no message in the queue, print a message and exit    
             else:
                 print("No message in the queue")
-                exit(1)
-                
+                continue
+        
         # Handle any errors that may occur connecting to SQS
         except ClientError as e:
             print(e.response['Error']['Message'])
 
+    print(messages_dict)
+    # sort the dic
+    sorted_dict = dict(sorted(messages_dict.items()))
+    print(sorted_dict)
+    # fetch out the VALUES of the dict, once it's sorted (another for loop)
+    for each in sorted_dict:
+        final_message = sorted_dict[each]
+        print(final_message, end = ' ')
 # Trigger the function
 if __name__ == "__main__":
     get_message()
+
+# going to simply enable this after i check in with the professor
+"""
+def delete_message(handle):
+    try:
+        # Delete message from SQS queue
+        sqs.delete_message(
+            QueueUrl=url,
+            ReceiptHandle=handle
+        )
+        print("Message deleted")
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        """
